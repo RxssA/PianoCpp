d7d32b4001ba901e95ac16afe383da8f19d2b18c
Rxss <rossarmo99@gmail.com>
2025-01-22 20:33:59 +0000

    First Push

---
+++ PianoCpp/VirtualPiano.cpp
@@ -0,0 +1,17 @@
+/*
+Ross Armitage
+22/02/2025
+*/
+
+#include <iostream>
+#include <dos.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define SHOW 1
+#define HIDE 2
+union REGS input, output;
+
+class piano {
+
+};
